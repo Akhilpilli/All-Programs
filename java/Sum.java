f0ce7d842bd6edeb2eb5c8b30a60aa0414f98682
Akhil Pilli <akhilpilli3198@gmail.com>
2019-01-31 11:09:06 +0000

    codenvy

--- java/Sum.java
+++ java/Sum.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-  	int FirstNumbe, SecondNumber, Sum;
+  	int FirstNumber, SecondNumber, Sum;
     Scanner in = new Scanner (System.in);
     System.out.print("To add two number's enter first number :");
+    FirstNumber = in.nextInt();
     System.out.print("Enter second number :");
     SecondNumber = in.nextInt();
-    Sum = FirstNumbe + SecondNumber;
-    System.out.println("The Sum of " + FirstNumbe + " and " + SecondNumber + " is " + Sum + ".");
+    Sum = FirstNumber + SecondNumber;
+    System.out.println("The Sum of " + FirstNumber + " and " + SecondNumber + " is " + Sum + ".");
     
   }
 }
